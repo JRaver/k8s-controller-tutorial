af184bd8f6f1ebbfb1a34a2081cc276fbdde98c4
Yevhen Artemenko <eartemenko@playtika.com>
2025-06-21 10:24:30 +0300

    Fwdays Add tests for the lab 1-4

---
+++ cmd/server_test.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestServerCmd(t *testing.T) {
+	if serverCmd == nil {
+		t.Errorf("serverCmd should not be defined")
+	}
+
+	if serverCmd.Use != "server" {
+		t.Errorf("serverCmd.Use should be 'server'")
+	}
+	
+	if serverCmd.Flags().Lookup("port") == nil {
+		t.Errorf("expected port flag to be defined")
+	}
+}
